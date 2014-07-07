182cf4128c412e47960b984d224390792a093a70
Josep Sansalvador Balaguer <josep@androidsx.com>
2014-07-07 23:40:56 +0200

    Added New York location for do tests at Current Time

--- rain-notifications/mobile/src/main/java/com/androidsx/rain_notifications/forecast_mobile.java
+++ rain-notifications/mobile/src/main/java/com/androidsx/rain_notifications/forecast_mobile.java
@@ -11,24 +11,18 @@ import android.widget.TextView;
 import com.forecast.io.network.responses.INetworkResponse;
 import com.forecast.io.network.responses.NetworkResponse;
 import com.forecast.io.toolbox.NetworkServiceTask;
-import com.forecast.io.v1.network.responses.InterestingStormsResponse;
-import com.forecast.io.v1.network.responses.MultiplePointsTimesResponse;
-import com.forecast.io.v1.network.services.HourlyForecastService;
-import com.forecast.io.v1.network.services.InterestingStormsService;
-import com.forecast.io.v1.network.services.MultiplePointsService;
-import com.forecast.io.v1.transfer.LatLng;
 import com.forecast.io.v2.network.services.ForecastService;
+import com.forecast.io.v2.transfer.LatLng;
 
 
 public class forecast_mobile extends Activity implements View.OnClickListener {
 
-    private static final String API_KEY = "2358ea11167b29a20c2fb02d634b9d3f";
+    private static final String API_KEY = "f1fd27e70564bd6765bf40b3497cbf4f";
+    private static final Double NEW_YORK_LAT = 40.72228267283148;
+    private static final Double NEW_YORK_LON = -73.9434814453125;
 
     private Button btn_call;
-    private TextView txt_response1;
-    private TextView txt_response2;
-    private TextView txt_response3;
-    private TextView txt_response4;
+    private TextView txt_response;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,100 +30,44 @@ public class forecast_mobile extends Activity implements View.OnClickListener {
         setContentView(R.layout.activity_forecast_mobile);
 
         setupUI();
+        onClick(null);
     }
 
     private void setupUI() {
         btn_call = (Button) findViewById(R.id.btn_call);
-        txt_response1 = (TextView) findViewById(R.id.txt_response1);
-        txt_response2 = (TextView) findViewById(R.id.txt_response2);
-        txt_response3 = (TextView) findViewById(R.id.txt_response3);
-        txt_response4 = (TextView) findViewById(R.id.txt_response4);
+        txt_response = (TextView) findViewById(R.id.txt_response);
 
         btn_call.setOnClickListener(this);
     }
 
     private void forecast_calls() {
-        HourlyForecastService.Request request = HourlyForecastService.Request.newBuilder( API_KEY )
-                .setForecastType( HourlyForecastService.ForecastType.FORECAST )
-                .setLatitude( 37.422006 )
-                .setLongitude(-122.084095)
-                .build();
+        LatLng.Builder builderL = LatLng.newBuilder();
+        builderL.setLatitude(NEW_YORK_LAT).setLongitude(NEW_YORK_LON).setTime(System.currentTimeMillis()/1000).build();
+        LatLng latlng = new LatLng(builderL);
+
+        ForecastService.Builder builderF = ForecastService.Request.newBuilder( API_KEY );
+        builderF.setLatLng(latlng).build();
+        ForecastService.Request request = new ForecastService.Request(builderF);
 
         new NetworkServiceTask() {
 
             @Override
-            protected void onPostExecute( INetworkResponse network ) {
-                if ( network == null || network.getStatus() == NetworkResponse.Status.FAIL ) {
-                    txt_response1.setText("HOURLY ERROR: " + network.getStatus().toString());
-                    return;
-                }
-
-                HourlyForecastService.Response response = (HourlyForecastService.Response) network;
-                txt_response1.setText("HOURLY OK: " + response.getSkyResponse().getCurrentSummary());
+            protected void onPreExecute() {
+                super.onPreExecute();
+                txt_response.setText("Loading...");
             }
-
-        }.execute( request );
-
-        MultiplePointsService.Request multiple = MultiplePointsService.Request.newBuilder( API_KEY )
-                .setPoint( LatLng.newBuilder()
-                        .setLatitude(37.422006)
-                        .setLongitude(-122.084095)
-                        .setTime(1364956418))
-                .setPoint( LatLng.newBuilder()
-                        .setLatitude( 37.422006 )
-                        .setLongitude( -122.084095 )
-                        .setTime( 1364956418 ) )
-                .build();
-
-        new NetworkServiceTask() {
 
             @Override
             protected void onPostExecute( INetworkResponse network ) {
                 if ( network == null || network.getStatus() == NetworkResponse.Status.FAIL ) {
-                    txt_response2.setText("MULTI POINT ERROR: " + network.getStatus().toString());
+                    txt_response.setText("HOURLY ERROR: " + network.getStatus().toString());
                     return;
                 }
 
-                MultiplePointsService.Response response = (MultiplePointsService.Response) network;
-
-                MultiplePointsTimesResponse points = response.getMultiplePointsTimes();
-                txt_response2.setText("MULTI POINT OK: " + points.getSkyPrecipitation() != null ?
-                        points.getSkyPrecipitation().get( 0 ).getType() : "NO MULTIPLE POINTS AND TIMES");
+                ForecastService.Response response = (ForecastService.Response) network;
+                txt_response.setText("HOURLY OK: " + response.getForecast().getCurrently().getSummary());
             }
 
-        }.execute( multiple );
-
-        new NetworkServiceTask() {
-
-            @Override
-            protected void onPostExecute( INetworkResponse network ) {
-                if ( network == null || network.getStatus() == NetworkResponse.Status.FAIL ) {
-                    txt_response3.setText("INTERESTING STORMS ERROR: " + network.getStatus().toString());
-                    return;
-                }
-
-                InterestingStormsService.Response response = (InterestingStormsService.Response) network;
-
-                InterestingStormsResponse storms = response.getInterestingStorms();
-                txt_response3.setText("INTERESTING STORMS OK: " + storms.getInterestingStorms() != null ?
-                        storms.getInterestingStorms().get( 0 ).getCity() : "NO INTERESTING STORMS");
-            }
-
-        }.execute(InterestingStormsService.Request.newBuilder(API_KEY).build());
-
-        new NetworkServiceTask() {
-
-            @Override
-            protected void onPostExecute( INetworkResponse network ) {
-                if ( network == null || network.getStatus() == NetworkResponse.Status.FAIL ) {
-                    txt_response4.setText("FORECAST ERROR: " + network.getStatus().toString());
-                    return;
-                }
-
-                ForecastService.Response response = ( ForecastService.Response ) network;
-                txt_response4.setText("FORECAST OK: " + response.getForecast() != null ?
-                        response.getForecast().getCurrently().getSummary() : "FORECAST");
-            }
         }.execute( request );
     }
 
