3635b5d6ed9e0cebdcd0fbfb02bc283b818da81a
Josep Sansalvador Balaguer <josep@androidsx.com>
2014-07-21 16:41:05 +0200

    Added debugHelper for clean weatherService code

--- rain-notifications/mobile/src/main/java/com/androidsx/rainnotifications/service/WeatherService.java
+++ rain-notifications/mobile/src/main/java/com/androidsx/rainnotifications/service/WeatherService.java
@@ -10,6 +10,7 @@ import android.os.IBinder;
 import android.util.Log;
 
 import com.androidsx.rainnotifications.ForecastAnalyzer;
+import com.androidsx.rainnotifications.util.DebugHelper;
 import com.androidsx.rainnotifications.util.SchedulerHelper;
 import com.androidsx.rainnotifications.util.AddressHelper;
 import com.androidsx.rainnotifications.util.AnalyzerHelper;
@@ -137,83 +138,11 @@ public class WeatherService extends Service {
         Log.d(TAG, "Weather Observer update...");
 
         if(dpRain != null && currently != null) {
-            displayDebugResults(dpRain.getTime() * 1000, dpRain.getIcon(), currently.getIcon(), Constants.ForecastIO.Icon.RAIN);
+            DebugHelper.displayDebugResults(this, shared, dpRain.getTime() * 1000, dpRain.getIcon(), currently.getIcon(), Constants.ForecastIO.Icon.RAIN);
         } else if(dpRain == null && currently != null) {
-            displayDebugResults(0, "", currently.getIcon(), Constants.ForecastIO.Icon.RAIN);
+            DebugHelper.displayDebugResults(this, shared, 0, "", currently.getIcon(), Constants.ForecastIO.Icon.RAIN);
         }
 
         stopSelf();
-    }
-
-    private void displayDebugResults(long nextIconTime, String nextForecastIcon, String currentlyIcon, String searchingIcon) {
-
-        String history = shared.getForecastHistory();
-
-        String update = "";
-        String currentTime = DateHelper
-                .formatTimeMadrid(System.currentTimeMillis());
-        String nextApiCall = DateHelper
-                .formatTimeMadrid(SchedulerHelper.nextApiCallTime(nextIconTime));
-        if(nextIconTime == 0) {
-            update = "\nSearching: " + searchingIcon + "\n\nCurrently: " + currentlyIcon +
-                    " at "+ currentTime +
-                    "\n\nNo changes expected until tomorrow." +
-                    "\n\nNext API call at: " + nextApiCall;
-        }
-        else {
-            String deltaTime = DateHelper
-                    .deltaTime(nextIconTime, System.currentTimeMillis());
-
-            String forecastTime = DateHelper
-                    .formatTimeMadrid(nextIconTime);
-
-            if(AnalyzerHelper.compareTo(nextForecastIcon, searchingIcon)) {
-                update = "\nFound: " + nextForecastIcon + "\n\nCurrently: " + currentlyIcon +
-                        "\nat "+ currentTime +
-                        "\n\n" + nextForecastIcon + " expected at " + forecastTime +
-                        " \n" + deltaTime + ".\n\nNext API call at: " + nextApiCall;
-            } else {
-                update = "\nSearching: " + searchingIcon + "\n\nCurrently: " + currentlyIcon +
-                        "\nat "+ currentTime +
-                        "\n\n" + nextForecastIcon + " expected at " + forecastTime +
-                        " \n" + deltaTime + ".\n\nNext API call at: " + nextApiCall;
-            }
-        }
-        history += update + "\n--------------------";
-        if(nextIconTime == 0) {
-            update = "No changes expected until tomorrow." +
-                    "\n\nNext API call at: " + nextApiCall + "\n";
-            shared.setCurrentForecastIcon(currentlyIcon);
-            shared.setNextForecastIcon(currentlyIcon);
-        }
-        else {
-            String deltaTime = DateHelper
-                    .deltaTime(nextIconTime, System.currentTimeMillis());
-            if(AnalyzerHelper.compareTo(nextForecastIcon, searchingIcon)) {
-                update = deltaTime + ".\n\nNext API call at: " + nextApiCall + "\n";
-            } else {
-                update = deltaTime + ".\n\nNext API call at: " + nextApiCall + "\n";
-            }
-            shared.setCurrentForecastIcon(currentlyIcon);
-            shared.setNextForecastIcon(nextForecastIcon);
-        }
-        Log.d(TAG, ".\n" + update);
-
-        shared.setCurrentForecast(update);
-        shared.setForecastHistory(history);
-
-        String deltaTime = DateHelper
-                .deltaTime(nextIconTime, System.currentTimeMillis());
-        String expectedTime = DateHelper
-                .formatTimeMadrid(nextIconTime);
-        if(!currentlyIcon.equals(Constants.ForecastIO.Icon.RAIN) && nextForecastIcon.equals(Constants.ForecastIO.Icon.RAIN)) {
-            NotificationHelper.sendNotification(this, 0,
-                    WeatherIconHelper.getWeatherIcon(nextForecastIcon),
-                    "Rain expected " + deltaTime + " at " + expectedTime);
-        } else if(currentlyIcon.equals(Constants.ForecastIO.Icon.RAIN) && !nextForecastIcon.equals(Constants.ForecastIO.Icon.RAIN)) {
-            NotificationHelper.sendNotification(this, 0,
-                    WeatherIconHelper.getWeatherIcon(nextForecastIcon),
-                    "Stop raining expected " + deltaTime + " at " + expectedTime);
-        }
     }
 }
---
+++ rain-notifications/mobile/src/main/java/com/androidsx/rainnotifications/util/DebugHelper.java
@@ -0,0 +1,84 @@
+package com.androidsx.rainnotifications.util;
+
+import android.content.Context;
+import android.util.Log;
+
+public class DebugHelper {
+
+    private static final String TAG = DebugHelper.class.getSimpleName();
+
+    private DebugHelper() {
+        //No-instantiate
+    }
+
+    public static void displayDebugResults(Context context, SharedPrefsHelper shared, long nextIconTime, String nextForecastIcon, String currentlyIcon, String searchingIcon) {
+        String history = shared.getForecastHistory();
+
+        String update = "";
+        String currentTime = DateHelper
+                .formatTimeMadrid(System.currentTimeMillis());
+        String nextApiCall = DateHelper
+                .formatTimeMadrid(SchedulerHelper.nextApiCallTime(nextIconTime));
+        if(nextIconTime == 0) {
+            update = "\nSearching: " + searchingIcon + "\n\nCurrently: " + currentlyIcon +
+                    " at "+ currentTime +
+                    "\n\nNo changes expected until tomorrow." +
+                    "\n\nNext API call at: " + nextApiCall;
+        }
+        else {
+            String deltaTime = DateHelper
+                    .deltaTime(nextIconTime, System.currentTimeMillis());
+
+            String forecastTime = DateHelper
+                    .formatTimeMadrid(nextIconTime);
+
+            if(AnalyzerHelper.compareTo(nextForecastIcon, searchingIcon)) {
+                update = "\nFound: " + nextForecastIcon + "\n\nCurrently: " + currentlyIcon +
+                        "\nat "+ currentTime +
+                        "\n\n" + nextForecastIcon + " expected at " + forecastTime +
+                        " \n" + deltaTime + ".\n\nNext API call at: " + nextApiCall;
+            } else {
+                update = "\nSearching: " + searchingIcon + "\n\nCurrently: " + currentlyIcon +
+                        "\nat "+ currentTime +
+                        "\n\n" + nextForecastIcon + " expected at " + forecastTime +
+                        " \n" + deltaTime + ".\n\nNext API call at: " + nextApiCall;
+            }
+        }
+        history += update + "\n--------------------";
+        if(nextIconTime == 0) {
+            update = "No changes expected until tomorrow." +
+                    "\n\nNext API call at: " + nextApiCall + "\n";
+            shared.setCurrentForecastIcon(currentlyIcon);
+            shared.setNextForecastIcon(currentlyIcon);
+        }
+        else {
+            String deltaTime = DateHelper
+                    .deltaTime(nextIconTime, System.currentTimeMillis());
+            if(AnalyzerHelper.compareTo(nextForecastIcon, searchingIcon)) {
+                update = deltaTime + ".\n\nNext API call at: " + nextApiCall + "\n";
+            } else {
+                update = deltaTime + ".\n\nNext API call at: " + nextApiCall + "\n";
+            }
+            shared.setCurrentForecastIcon(currentlyIcon);
+            shared.setNextForecastIcon(nextForecastIcon);
+        }
+        Log.d(TAG, ".\n" + update);
+
+        shared.setCurrentForecast(update);
+        shared.setForecastHistory(history);
+
+        String deltaTime = DateHelper
+                .deltaTime(nextIconTime, System.currentTimeMillis());
+        String expectedTime = DateHelper
+                .formatTimeMadrid(nextIconTime);
+        if(!currentlyIcon.equals(Constants.ForecastIO.Icon.RAIN) && nextForecastIcon.equals(Constants.ForecastIO.Icon.RAIN)) {
+            NotificationHelper.sendNotification(context, 0,
+                    WeatherIconHelper.getWeatherIcon(nextForecastIcon),
+                    "Rain expected " + deltaTime + " at " + expectedTime);
+        } else if(currentlyIcon.equals(Constants.ForecastIO.Icon.RAIN) && !nextForecastIcon.equals(Constants.ForecastIO.Icon.RAIN)) {
+            NotificationHelper.sendNotification(context, 0,
+                    WeatherIconHelper.getWeatherIcon(nextForecastIcon),
+                    "Stop raining expected " + deltaTime + " at " + expectedTime);
+        }
+    }
+}
