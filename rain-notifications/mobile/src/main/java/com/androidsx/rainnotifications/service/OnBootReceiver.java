7e09969ef4d93b75272018e0d5abcea133f9f98b
Josep Sansalvador Balaguer <josep@androidsx.com>
2014-07-24 15:48:43 +0200

    Updated doc in OnBootReceiver

--- rain-notifications/mobile/src/main/java/com/androidsx/rainnotifications/service/OnBootReceiver.java
+++ rain-notifications/mobile/src/main/java/com/androidsx/rainnotifications/service/OnBootReceiver.java
@@ -9,12 +9,6 @@ import com.androidsx.rainnotifications.util.SchedulerHelper;
 
 /**
  * This receiver is responsible to init LocationService on system boot, for start all the processes.
- *
- * LocationService -> registers an alarm for call again LocationService with the previous location.
- * LocationService -> starts WeatherService.
- *
- * WeatherService -> make API call for receive the forecast.
- * WeatherService -> registers an alarm determined by the next significant forecast time.
  */
 
 public class OnBootReceiver extends BroadcastReceiver {
