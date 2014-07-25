680d161540d871918f3690ca77345fae0fd2410f
Josep Sansalvador Balaguer <josep@androidsx.com>
2014-07-25 18:32:31 +0200

    Updated OnBootReceiver for new ForecastService

--- rain-notifications/mobile/src/main/java/com/androidsx/rainnotifications/service/OnBootReceiver.java
+++ rain-notifications/mobile/src/main/java/com/androidsx/rainnotifications/service/OnBootReceiver.java
@@ -3,6 +3,7 @@ package com.androidsx.rainnotifications.service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 
 /**
  * This receiver is responsible to init LocationService on system boot, for start all the processes.
@@ -11,6 +12,8 @@ import android.content.Intent;
 public class OnBootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        context.startService(new Intent(context, LocationService.class));
+        Bundle mBundle = new Bundle();
+        mBundle.putInt(ForecastService.EXTRA_ALARM_TYPE, ForecastService.LOCATION_ALARM_ID);
+        context.startService(new Intent(context, ForecastService.class).putExtras(mBundle));
     }
 }
