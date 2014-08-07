1a7d7d34b110b19e2c67b14396e747c8e3dd002f
Josep Sansalvador Balaguer <josep@androidsx.com>
2014-08-07 18:04:41 +0200

    New class for manage wear connections

---
+++ rain-notifications/mobile/src/main/java/com/androidsx/rainnotifications/WearManager.java
@@ -0,0 +1,94 @@
+package com.androidsx.rainnotifications;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+
+import com.androidsx.rainnotifications.model.Forecast;
+import com.androidsx.rainnotifications.model.Weather;
+import com.androidsx.rainnotifications.model.WeatherType;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.Asset;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public abstract class WearManager implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private static final String KEY_TITLE = "key_title";
+    private static final String KEY_MESSAGE = "key_message";
+    private static final String KEY_IMAGE = "key_image";
+
+    private GoogleApiClient mGoogleApiClient;
+    private Context context;
+
+    public WearManager(Context context) {
+        mGoogleApiClient = new GoogleApiClient.Builder(context)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        this.context = context;
+    }
+
+    public void connect() {
+        if (!mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    public void sendNotification(String message, Weather currentWeather, Forecast forecast){
+        if (mGoogleApiClient.isConnected()) {
+            PutDataMapRequest putDataMapRequest = PutDataMapRequest.create("/forecast");
+            // Add data to the request
+            putDataMapRequest.getDataMap().putString(KEY_TITLE,
+                    context.getResources().getString(R.string.app_name));
+
+            int icon = Constants.FORECAST_ICONS.containsKey(forecast.getForecastedWeather().getType())
+                    ? Constants.FORECAST_ICONS.get(forecast.getForecastedWeather().getType())
+                    : Constants.FORECAST_ICONS.get(WeatherType.UNKNOWN);
+
+            putDataMapRequest.getDataMap().putAsset(KEY_IMAGE, getAssetFromDrawable(icon));
+            PutDataRequest request = putDataMapRequest.asPutDataRequest();
+
+            Wearable.DataApi.putDataItem(mGoogleApiClient, request)
+                    .setResultCallback(new ResultCallback<DataApi.DataItemResult>() {
+                        @Override
+                        public void onResult(DataApi.DataItemResult dataItemResult) {
+
+                        }
+                    });
+        }
+    }
+
+    @Override
+    public abstract void onConnected(Bundle bundle);
+
+    @Override
+    public abstract void onConnectionSuspended(int i);
+
+    @Override
+    public abstract void onConnectionFailed(ConnectionResult connectionResult);
+
+    private Asset getAssetFromDrawable(int drawable) {
+        Bitmap iconB = BitmapFactory.decodeResource(context.getResources(), drawable);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        iconB.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        Asset asset = null;
+        try {
+            stream.flush();
+            asset = Asset.createFromBytes(stream.toByteArray());
+            stream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return asset;
+    }
+}
