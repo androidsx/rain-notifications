9eb012139e0d27549485c928c184d34006d3416a
Josep Sansalvador Balaguer <josep@androidsx.com>
2014-08-11 21:25:08 +0200

    Added common-library for constants

---
+++ rain-notifications/common-library/src/main/java/com/androidsx/common_library/Constants.java
@@ -0,0 +1,20 @@
+package com.androidsx.common_library;
+
+public class Constants {
+
+    public static final String WEAR_PATH = "/forecast";
+
+    public static class Extras {
+        public static final String EXTRA_TITLE = "key_title";
+        public static final String EXTRA_MESSAGE = "key_message";
+        public static final String EXTRA_CURRENT_ICON = "key_current_image";
+        public static final String EXTRA_FORECAST_ICON = "key_forecast_image";
+    }
+
+    public static class Keys {
+        public static final String KEY_TITLE = "key_title";
+        public static final String KEY_MESSAGE = "key_message";
+        public static final String KEY_CURRENT_ICON = "key_current_image";
+        public static final String KEY_FORECAST_ICON = "key_forecast_image";
+    }
+}
