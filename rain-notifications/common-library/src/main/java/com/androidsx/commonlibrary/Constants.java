e1ccad449c7c07c9477561004fd0ac031f6d3e33
Josep Sansalvador Balaguer <josep@androidsx.com>
2014-08-11 21:29:51 +0200

    Fixed string constants

--- rain-notifications/common-library/src/main/java/com/androidsx/commonlibrary/Constants.java
+++ rain-notifications/common-library/src/main/java/com/androidsx/commonlibrary/Constants.java
@@ -5,10 +5,10 @@ public class Constants {
     public static final String WEAR_PATH = "/forecast";
 
     public static class Extras {
-        public static final String EXTRA_TITLE = "key_title";
-        public static final String EXTRA_MESSAGE = "key_message";
-        public static final String EXTRA_CURRENT_ICON = "key_current_image";
-        public static final String EXTRA_FORECAST_ICON = "key_forecast_image";
+        public static final String EXTRA_TITLE = "extra_title";
+        public static final String EXTRA_MESSAGE = "extra_message";
+        public static final String EXTRA_CURRENT_ICON = "extra_current_image";
+        public static final String EXTRA_FORECAST_ICON = "extra_forecast_image";
     }
 
     public static class Keys {
