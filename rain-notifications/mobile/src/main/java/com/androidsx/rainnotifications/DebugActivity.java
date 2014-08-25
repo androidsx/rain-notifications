01094400cedb621cbdf08d5c1ed984e41fbedba2
Pablo Pera <pablo.sx@gmail.com>
2014-08-24 20:28:44 -0400

    New style of notifications: non-custom, but extended for wear

--- rain-notifications/mobile/src/main/java/com/androidsx/rainnotifications/DebugActivity.java
+++ rain-notifications/mobile/src/main/java/com/androidsx/rainnotifications/DebugActivity.java
@@ -1,9 +1,13 @@
 package com.androidsx.rainnotifications;
 
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -11,6 +15,8 @@ import android.widget.TextView;
 import com.androidsx.rainnotifications.model.WeatherType;
 import com.androidsx.rainnotifications.service.WeatherService;
 import com.androidsx.rainnotifications.util.SharedPrefsHelper;
+
+import timber.log.Timber;
 
 public class DebugActivity extends Activity {
     private TextView locationTextView;
@@ -55,6 +61,44 @@ public class DebugActivity extends Activity {
     /** Linked to the button in the XML layout. */
     public void refreshUi(View view) {
         updateUiFromPrefs();
+    }
+
+    /** Linked to the button in the XML layout. */
+    public void showNotification(View view) {
+        Timber.d("Show a random notification");
+
+        final int notificationId = 002;
+
+        // Main intent for the notification (click for mobile, swipe left and click for wear)
+        Intent viewIntent = new Intent(this, ForecastMobile.class);
+        PendingIntent viewPendingIntent =
+                PendingIntent.getActivity(this, 0, viewIntent, 0);
+
+        // Big style for the notification. It only matters for mobile AFAIK, to show several lines
+        NotificationCompat.BigTextStyle bigTextStyle = new NotificationCompat.BigTextStyle()
+                .setBigContentTitle(getString(R.string.notif_title))
+                .bigText(getString(R.string.notif_long_text_fake));
+
+        // Additional functionality for wear
+        NotificationCompat.WearableExtender wearableExtender = new NotificationCompat.WearableExtender()
+                        .setHintHideIcon(true)
+                        .setBackground(BitmapFactory.decodeResource(getResources(), R.drawable.notification_background_fake))
+                        .setContentIcon(R.drawable.owl_sunny_fake);
+
+        // Finally configure the notification builder
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_launcher) // Compulsory. Only for the phone AFAIK. TODO: follow guidelines: gray
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.owl_sunny_fake))
+                        .setContentTitle(getString(R.string.notif_title))
+                        .setContentText(getString(R.string.notif_long_text_fake))
+                        .setContentIntent(viewPendingIntent)
+                        .setStyle(bigTextStyle)
+                        .extend(wearableExtender)
+                ;
+
+        // Build the notification and launch it
+        NotificationManagerCompat.from(this).notify(notificationId, notificationBuilder.build());
     }
 
     /**
