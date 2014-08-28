51d054f75d7500e6d451e5375004d195313a3a95
Josep Sansalvador Balaguer <josep@androidsx.com>
2014-08-28 21:48:58 +0200

    Deleted unused imports and variables

--- rain-notifications/mobile/src/main/java/com/androidsx/rainnotifications/ForecastChecker.java
+++ rain-notifications/mobile/src/main/java/com/androidsx/rainnotifications/ForecastChecker.java
@@ -1,37 +1,20 @@
 package com.androidsx.rainnotifications;
 
-import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 
 import com.androidsx.rainnotifications.forecast_io.ForecastIoNetworkServiceTask;
 import com.androidsx.rainnotifications.forecast_io.ForecastIoRequest;
-import com.androidsx.rainnotifications.model.Forecast;
 import com.androidsx.rainnotifications.model.ForecastTable;
 import com.androidsx.rainnotifications.model.Weather;
-import com.androidsx.rainnotifications.model.WeatherType;
-import com.androidsx.rainnotifications.model.util.UiUtil;
 import com.androidsx.rainnotifications.service.WeatherService;
 import com.androidsx.rainnotifications.util.AlarmHelper;
 import com.androidsx.rainnotifications.util.LocationHelper;
 
-import org.joda.time.DateTimeConstants;
-import org.joda.time.Interval;
-import org.joda.time.Period;
-
-import java.util.List;
-
-import timber.log.Timber;
-
 public class ForecastChecker {
 
     private static final String TAG = ForecastChecker.class.getSimpleName();
-
-    private static final long WEATHER_REPEATING_TIME_MILLIS = 10 * DateTimeConstants.MILLIS_PER_MINUTE;
-    public static final long TEN_MINUTES_MILLIS = 10 * DateTimeConstants.MILLIS_PER_MINUTE;
-    private static final long ONE_HOUR_MILLIS = 1 * 60 * DateTimeConstants.MILLIS_PER_MINUTE;
-    private static final long DEFAULT_EXTRA_TIME_MILLIS = 1 * 60 * DateTimeConstants.MILLIS_PER_MINUTE;
 
     /**
      * Asynchronous method that call for obtain the weather forecast into a determined location.
