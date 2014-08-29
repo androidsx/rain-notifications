4dcf46430913d72aaa61d456142f2601361a4beb
Josep Sansalvador Balaguer <josep@androidsx.com>
2014-08-30 00:15:27 +0200

    Removed unused imports

--- rain-notifications/mobile/src/main/java/com/androidsx/rainnotifications/util/NotificationHelper.java
+++ rain-notifications/mobile/src/main/java/com/androidsx/rainnotifications/util/NotificationHelper.java
@@ -1,11 +1,9 @@
 package com.androidsx.rainnotifications.util;
 
-import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 
