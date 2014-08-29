484af6031a211d0a330546f37516ab3cb22659cc
Josep Sansalvador Balaguer <josep@androidsx.com>
2014-08-29 20:55:28 +0200

    Fixed alertGenerator code

--- rain-notifications/alert-generator/src/main/java/com/androidsx/rainnotifications/alert/AlertGenerator.java
+++ rain-notifications/alert-generator/src/main/java/com/androidsx/rainnotifications/alert/AlertGenerator.java
@@ -68,7 +68,7 @@ public class AlertGenerator {
      */
     private AlertMessage generateAlertMessage(Weather currentWeather, Forecast forecast) {
         if (forecast == null) {
-            return new AlertMessage("No changes expected for a while.") //TODO: message that refers to no forecast expected in a few hours
+            return new AlertMessage("No changes expected for a while." //TODO: message that refers to no forecast expected in a few hours
                 + " At the moment, it is " + currentWeather);
         }
         final Period periodFromNow = forecast.getTimeFromNow().toPeriod();
