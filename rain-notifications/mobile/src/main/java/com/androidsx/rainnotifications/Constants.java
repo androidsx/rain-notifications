aaaa356effcd66a3daf76cab99d104283912d7c9
Pablo Pera <pablo.sx@gmail.com>
2014-08-30 21:39:10 -0400

    Heavily simplified the debug activity

--- rain-notifications/mobile/src/main/java/com/androidsx/rainnotifications/Constants.java
+++ rain-notifications/mobile/src/main/java/com/androidsx/rainnotifications/Constants.java
@@ -10,17 +10,11 @@ public class Constants {
         public static final int LOCATION_ID = 0;
         public static final int WEATHER_ID = 1;
     }
-    public static class Extras {
-        public static final String EXTRA_LAT = "extra_lat";
-        public static final String EXTRA_LON = "extra_lon";
-        public static final String EXTRA_ADDRESS = "extra_address";
-    }
     public static class SharedPref {
         public static final String ADDRESS = "address";
         public static final String CURRENTLY = "currently";
         public static final String CURRENTLY_ICON = "currently_icon";
         public static final String NEXT_FORECAST_ICON = "next_forecast_icon";
-        public static final String FIRST_TIME_EXECUTION = "first_time_execution";
     }
     public static class Assets {
         public static final String ROBOTO_REGULAR_URL = "roboto/Roboto-Regular.ttf";
--- rain-notifications/mobile/src/main/java/com/androidsx/rainnotifications/util/SharedPrefsHelper.java
+++ rain-notifications/mobile/src/main/java/com/androidsx/rainnotifications/util/SharedPrefsHelper.java
@@ -3,46 +3,12 @@ package com.androidsx.rainnotifications.util;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-import com.androidsx.rainnotifications.Constants.SharedPref;
-
 public class SharedPrefsHelper {
 
     public static final String SHARED_RAIN = "shared_rain";
 
     private SharedPrefsHelper() {
-        //No-instantiate
-    }
-
-    public static void setNextForecast(String current, SharedPreferences.Editor editor) {
-        editor.putString(SharedPref.CURRENTLY, current).commit();
-    }
-
-    public static String getNextForecast(SharedPreferences sharedPref) {
-        return sharedPref.getString(SharedPref.CURRENTLY, "");
-    }
-
-    public static void setCurrentForecastIcon(int icon, SharedPreferences.Editor editor) {
-        editor.putInt(SharedPref.CURRENTLY_ICON, icon).commit();
-    }
-
-    public static int getCurrentForecastIcon(SharedPreferences sharedPref) {
-        return sharedPref.getInt(SharedPref.CURRENTLY_ICON, 0);
-    }
-
-    public static void setNextForecastIcon(int icon, SharedPreferences.Editor editor) {
-        editor.putInt(SharedPref.NEXT_FORECAST_ICON, icon).commit();
-    }
-
-    public static int getNextForecastIcon(SharedPreferences sharedPref) {
-        return sharedPref.getInt(SharedPref.NEXT_FORECAST_ICON, 0);
-    }
-
-    public static void setForecastAddress(String address, SharedPreferences.Editor editor) {
-        editor.putString(SharedPref.ADDRESS, address).commit();
-    }
-
-    public static String getForecastAddress(SharedPreferences sharedPref) {
-        return sharedPref.getString(SharedPref.ADDRESS, "");
+        // Non-instantiable
     }
 
     public static void saveIntValue(Context context, String key, int value) {
@@ -57,8 +23,7 @@ public class SharedPrefsHelper {
     }
 
     private static int getIntValueInternal(Context context, String key, int defaultValue) {
-        SharedPreferences config = context.getSharedPreferences(SharedPrefsHelper.SHARED_RAIN,
-                0);
+        SharedPreferences config = context.getSharedPreferences(SharedPrefsHelper.SHARED_RAIN, 0);
         return config.getInt(key, defaultValue);
     }
 }
