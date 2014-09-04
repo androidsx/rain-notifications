28a6bb96fc8759f1c097e9697060517c995ec873
Josep Sansalvador Balaguer <josep@androidsx.com>
2014-09-04 21:48:09 +0200

    Added flag mechanism aproximation for choose forecast provider

--- mobile/src/main/java/com/androidsx/rainnotifications/util/ForecastChecker.java
+++ mobile/src/main/java/com/androidsx/rainnotifications/util/ForecastChecker.java
@@ -5,28 +5,47 @@ import android.content.Context;
 import com.androidsx.rainnotifications.forecast_io.ForecastIoNetworkServiceTask;
 import com.androidsx.rainnotifications.forecast_io.ForecastIoRequest;
 import com.androidsx.rainnotifications.model.ForecastTable;
+import com.androidsx.rainnotifications.wunderground.WundergroundNetworkServiceTask;
 
 /**
  * Class that calls for obtain the weather forecast.
  */
 public class ForecastChecker {
 
+    public static final String FORECAST_IO = "forecast-io";
+    public static final String WUNDERGROUND = "wunderground";
+
     /**
      * Asynchronous method that call for obtain the weather forecast into a determined location.
      */
-    public static void requestForecastForLocation(Context context, double latitude, double longitude,
+    public static void requestForecastForLocation(Context context, String provider, double latitude, double longitude,
                                                   final ForecastCheckerResultListener forecastCheckerResultListener) {
         if (LocationHelper.rightCoordinates(latitude, longitude)) {
-            new ForecastIoNetworkServiceTask() {
-                @Override
-                public void onRequestSuccess(ForecastTable forecastTable) {
-                    forecastCheckerResultListener.onForecastSuccess(forecastTable);
-                }
-                @Override
-                public void onRequestFailure() {
-                    forecastCheckerResultListener.onForecastFailure(new ForecastCheckerException());
-                }
-            }.execute(new ForecastIoRequest(latitude, longitude).getRequest());
+            if (provider.equals(FORECAST_IO)) {
+                new ForecastIoNetworkServiceTask() {
+                    @Override
+                    public void onRequestSuccess(ForecastTable forecastTable) {
+                        forecastCheckerResultListener.onForecastSuccess(forecastTable);
+                    }
+                    @Override
+                    public void onRequestFailure() {
+                        forecastCheckerResultListener.onForecastFailure(new ForecastCheckerException());
+                    }
+                }.execute(new ForecastIoRequest(latitude, longitude).getRequest());
+
+            } else if (provider.equals(WUNDERGROUND)) {
+                new WundergroundNetworkServiceTask() {
+                    @Override
+                    public void onRequestSuccess(ForecastTable forecastTable) {
+                        forecastCheckerResultListener.onForecastSuccess(forecastTable);
+                    }
+
+                    @Override
+                    public void onRequestFailure() {
+                        forecastCheckerResultListener.onForecastFailure(new ForecastCheckerException());
+                    }
+                }.execute(context, latitude, longitude);
+            }
         }
     }
 
