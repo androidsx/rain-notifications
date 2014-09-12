3bd861c6d29d007973a0eef3d38b3036120a4bd4
Josep Sansalvador Balaguer <josep@androidsx.com>
2014-09-12 17:22:29 +0200

    Updated ForecastApi for support sunrise/sunset params

--- weather-client-forecast-io/src/main/java/com/androidsx/rainnotifications/model/ForecastTableBuilder.java
+++ weather-client-forecast-io/src/main/java/com/androidsx/rainnotifications/model/ForecastTableBuilder.java
@@ -1,5 +1,7 @@
 package com.androidsx.rainnotifications.model;
 
+import android.util.Log;
+
 import com.forecast.io.v2.network.services.ForecastService;
 import com.forecast.io.v2.transfer.DataBlock;
 import com.forecast.io.v2.transfer.DataPoint;
@@ -23,6 +25,9 @@ public class ForecastTableBuilder {
         final DataBlock hourly = response.getForecast().getHourly();
         final DateTime currentTime = new DateTime(currently.getTime() * 1000);
 
+        final DateTime sunriseTime = new DateTime(response.getForecast().getDaily().getData().get(0).getSunriseTime());
+        final DateTime sunsetTime = new DateTime(response.getForecast().getDaily().getData().get(0).getSunsetTime());
+        Log.d("Forecast.io", "Sunrise: " + sunriseTime + " - Sunset: " + sunsetTime);
         final List<Forecast> allForecasts = new ArrayList<Forecast>();
         allForecasts.addAll(extractAllValidForecast(currentTime, minutely, Forecast.Granularity.MINUTE));
         allForecasts.addAll(extractAllValidForecast(currentTime, hourly, Forecast.Granularity.HOUR));
@@ -61,4 +66,22 @@ public class ForecastTableBuilder {
             return new Forecast(forecastedWeather, timeFromNow, granularity);
         }
     }
+
+    /*private static void setProperPhase(DateTime weatherTime, JSONObject sunPhase, Weather weather) {
+        DateTime sunriseTime = getSunPhaseTime(sunPhase, "sunrise");
+        DateTime sunsetTime = getSunPhaseTime(sunPhase, "sunset");
+        if (sunriseTime.isBefore(weatherTime) && weatherTime.isBefore(sunsetTime)) {
+            // Do nothing
+        } else {
+            weather.setPhase(WeatherPhase.NIGHT);
+        }
+    }
+
+    private static DateTime getSunPhaseTime(JSONObject sunPhase, String phase) {
+        final JSONObject sunrise = (JSONObject) sunPhase.get(phase);
+        DateTime sunPhaseTime = DateTime.now();
+        sunPhaseTime = sunPhaseTime.hourOfDay().setCopy(sunrise.getString("hour"));
+        sunPhaseTime = sunPhaseTime.minuteOfHour().setCopy(sunrise.getString("minute"));
+        return sunPhaseTime;
+    }*/
 }
