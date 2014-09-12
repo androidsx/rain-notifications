448d4c79f8b1b2730a3b8b937f67865a48f4a523
Josep Sansalvador Balaguer <josep@androidsx.com>
2014-09-12 15:48:35 +0200

    Added new supported weathers into wunderground

--- weather-client-wunderground/src/main/java/com/androidsx/rainnotifications/model/WundergroundWeatherTypeBuilder.java
+++ weather-client-wunderground/src/main/java/com/androidsx/rainnotifications/model/WundergroundWeatherTypeBuilder.java
@@ -3,6 +3,8 @@ package com.androidsx.rainnotifications.model;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.androidsx.rainnotifications.model.WeatherType.CLOUDY;
+import static com.androidsx.rainnotifications.model.WeatherType.PARTLY_CLOUDY;
 import static com.androidsx.rainnotifications.model.WeatherType.RAIN;
 import static com.androidsx.rainnotifications.model.WeatherType.SUNNY;
 import static com.androidsx.rainnotifications.model.WeatherType.UNKNOWN;
@@ -17,15 +19,15 @@ public class WundergroundWeatherTypeBuilder {
     final static private Map<String, WeatherType> ICON_TO_WEATHER_TYPE = new HashMap<String, WeatherType>() {{
         put("clear", SUNNY);
         put("sunny", SUNNY);
+        put("mostlysunny", SUNNY);
         put("rain", RAIN);
+        put("cloudy", CLOUDY);
+        put("mostlycloudy", CLOUDY);
+        put("partlycloudy", PARTLY_CLOUDY);
+        put("partlysunny", PARTLY_CLOUDY);
         //put("snow", SNOW); // Exists in Wunderground. Not supported by us yet
         //put("sleet", SLEET); // Exists in Wunderground. Not supported by us yet
         //put("fog", FOG); // Exists in Wunderground. Not supported by us yet
-        //put("cloudy", CLOUDY); // Exists in Wunderground. Not supported by us yet
-        //put("partlycloudy", ); // Exists in Wunderground. Not supported by us yet
-        //put("partlysunny", ); // Exists in Wunderground. Not supported by us yet
-        //put("mostlycloudy", ); // Exists in Wunderground. Not supported by us yet
-        //put("mostlysunny", ); // Exists in Wunderground. Not supported by us yet
         put("unknown", UNKNOWN);
     }};
 
