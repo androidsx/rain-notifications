f2f3ddad921921f6f50b74c4ae7c243d6be2ea64
Pablo Pera <pablo.sx@gmail.com>
2014-09-17 01:05:35 -0400

    First take at the tests for the data-driven alert generator

---
+++ tests/src/test/java/com/androidsx/rainnotifications/alert/AlertGeneratorConfigProcessingTest.java
@@ -0,0 +1,70 @@
+package com.androidsx.rainnotifications.alert;
+
+import com.androidsx.rainnotifications.model.Alert;
+import com.androidsx.rainnotifications.model.AlertLevel;
+import com.androidsx.rainnotifications.model.Forecast;
+import com.androidsx.rainnotifications.model.Weather;
+import com.androidsx.rainnotifications.model.WeatherType;
+import com.androidsx.rainnotifications.model.WeatherTypeMascots;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Tests for the processing of the JSON configuration in {@link AlertGenerator}.
+ */
+@Config(manifest = "./src/main/AndroidManifest.xml")
+@RunWith(RobolectricTestRunner.class)
+public class AlertGeneratorConfigProcessingTest {
+
+    @Test
+    public void testSameWeatherTransition() {
+        final AlertGenerator alertGenerator = new AlertGenerator(Robolectric.application);
+
+        // Create the alert (what would be in alerts.json)
+        final HashMap<String, List<String>> alertMessages = new HashMap<String, List<String>>();
+        alertMessages.put("en", Arrays.asList(new String("will stay clear")));
+        final Alert alert = new Alert(WeatherType.CLEAR, WeatherType.CLEAR, AlertLevel.NEVER_MIND, alertMessages, 0);
+
+        // Create the mascots (what would be in weatherTypeMascots.json)
+        final WeatherTypeMascots weatherTypeMascots = new WeatherTypeMascots(WeatherType.CLEAR, Arrays.asList("mascot_drawable_id"));
+
+        // Generate the actual alert
+        alertGenerator.init(Arrays.asList(new Alert[] {alert}), Arrays.asList(new WeatherTypeMascots[] {weatherTypeMascots}));
+        final Weather from = new Weather(WeatherType.CLEAR);
+        final Forecast to = new Forecast(new Weather(WeatherType.CLEAR), null, null);
+        final Alert generatedAlert = alertGenerator.generateAlert(from, to);
+
+        // Make sure the alert is exactly as we expected
+        Assert.assertEquals(alert, generatedAlert);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testTransitionNotDefinedThrowsException() {
+        final AlertGenerator alertGenerator = new AlertGenerator(Robolectric.application);
+
+        // Create the alert (what would be in alerts.json)
+        final HashMap<String, List<String>> alertMessages = new HashMap<String, List<String>>();
+        alertMessages.put("en", Arrays.asList(new String("will stay clear")));
+        final Alert alert = new Alert(WeatherType.CLEAR, WeatherType.CLEAR, AlertLevel.NEVER_MIND, alertMessages, 0);
+
+        // Create the mascots (what would be in weatherTypeMascots.json)
+        final WeatherTypeMascots weatherTypeMascots = new WeatherTypeMascots(WeatherType.CLEAR, Arrays.asList("mascot_drawable_id"));
+
+        // Generate the actual alert
+        alertGenerator.init(Arrays.asList(new Alert[] {alert}), Arrays.asList(new WeatherTypeMascots[] {weatherTypeMascots}));
+        final Weather from = new Weather(WeatherType.CLEAR);
+        final Forecast to = new Forecast(new Weather(WeatherType.RAIN), null, null);
+
+        alertGenerator.generateAlert(from, to); // This should throw an exception
+    }
+}
