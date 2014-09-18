ed01b84e851278e0c3d24b5392950287c60f4842
Josep Sansalvador Balaguer <josep@androidsx.com>
2014-09-18 16:06:36 +0200

    Added methods for save the set alarm time into shared preferences

--- mobile/src/main/java/com/androidsx/rainnotifications/util/SharedPrefsHelper.java
+++ mobile/src/main/java/com/androidsx/rainnotifications/util/SharedPrefsHelper.java
@@ -11,6 +11,17 @@ public class SharedPrefsHelper {
         // Non-instantiable
     }
 
+    public static void saveLongValue(Context context, String key, long value) {
+        SharedPreferences.Editor editor = context.getSharedPreferences(
+                SharedPrefsHelper.SHARED_RAIN, 0).edit();
+        editor.putLong(key, value);
+        editor.commit();
+    }
+
+    public static long getLongValue(Context context, String key) {
+        return SharedPrefsHelper.getLongValueInternal(context, key, 0);
+    }
+
     public static void saveIntValue(Context context, String key, int value) {
         SharedPreferences.Editor editor = context.getSharedPreferences(
                 SharedPrefsHelper.SHARED_RAIN, 0).edit();
@@ -25,5 +36,10 @@ public class SharedPrefsHelper {
     private static int getIntValueInternal(Context context, String key, int defaultValue) {
         SharedPreferences config = context.getSharedPreferences(SharedPrefsHelper.SHARED_RAIN, 0);
         return config.getInt(key, defaultValue);
+    }
+
+    private static long getLongValueInternal(Context context, String key, long defaultValue) {
+        SharedPreferences config = context.getSharedPreferences(SharedPrefsHelper.SHARED_RAIN, 0);
+        return config.getLong(key, defaultValue);
     }
 }
