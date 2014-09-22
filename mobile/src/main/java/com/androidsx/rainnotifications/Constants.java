f77bac8249d5f973a8745a224e4bf3a178f68d6e
Josep Sansalvador Balaguer <josep@androidsx.com>
2014-09-22 17:12:24 +0200

    Added new constant id for the day alarm

--- mobile/src/main/java/com/androidsx/rainnotifications/Constants.java
+++ mobile/src/main/java/com/androidsx/rainnotifications/Constants.java
@@ -3,6 +3,7 @@ package com.androidsx.rainnotifications;
 public class Constants {
     public static class AlarmId{
         public static final int WEATHER_ID = 1;
+        public static final int DAY_ALARM_ID = 2;
     }
     public static class Assets {
         public static final String ROBOTO_REGULAR_URL = "roboto/Roboto-Regular.ttf";
