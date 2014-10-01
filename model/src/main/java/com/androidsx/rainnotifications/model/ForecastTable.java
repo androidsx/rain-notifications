45c5f892051d0df3a7147a2bef1795c528c6f9c0
parodco <parodco@gmail.com>
2014-10-01 20:23:31 +0200

    Add javadoc for ForecastTable.fromForecastList

--- model/src/main/java/com/androidsx/rainnotifications/model/ForecastTable.java
+++ model/src/main/java/com/androidsx/rainnotifications/model/ForecastTable.java
@@ -12,13 +12,25 @@ import java.util.List;
  */
 public class ForecastTable {
 
-    private List<Forecast> mergedForecastList;
+    private List<Forecast> forecastList;
 
     /**
-     * @param forecastList An ordered list of forecasts without overlaps or gaps in their Intervals.
+     * Returns an appropiate {@link com.androidsx.rainnotifications.model.ForecastTable} for the given forecastList. It processed the given list as follows:
+     *
+     * <ol>
+     * <li>Remove not meaningful Forecasts from the list</li>
+     * <li>Merge consecutive Forecasts with same {@link com.androidsx.rainnotifications.model.WeatherType}</li>
+     * </ol>
+     *
+     * As a result of {@link #getMeaningfulForecastList(java.util.List)} and {@link #getMergedForecastList(java.util.List)}
+     * the gaps after remove not meaningful Forecast are filled by merging previous and subsequent Forecast if they has the
+     * same WeatherType. Otherwise the table contains gaps that are not taken into account for {@link com.androidsx.rainnotifications.model.DaySummary#fromForecastTable(ForecastTable)}
+     *
+     * @param forecastList An ordered list of {@link com.androidsx.rainnotifications.model.Forecast} without overlaps or gaps in their Intervals.
+     * @return {@link com.androidsx.rainnotifications.model.ForecastTable} if processed forecastList isn't empty, null in other case.
+     * @throws java.lang.IllegalArgumentException if the given forecastList is empty
      */
     public static ForecastTable fromForecastList(List<Forecast> forecastList) {
-
         if (forecastList.isEmpty()) {
             throw new IllegalArgumentException("The list of forecasts is empty. At least one forecast is needed");
         } else {
@@ -64,29 +76,29 @@ public class ForecastTable {
         return mergedForecastList;
     }
 
-    private ForecastTable(List<Forecast> mergedForecastList) {
-        this.mergedForecastList = mergedForecastList;
+    private ForecastTable(List<Forecast> forecastList) {
+        this.forecastList = forecastList;
     }
 
     public DateTime getStart() {
-        return mergedForecastList.get(0).getInterval().getStart();
+        return forecastList.get(0).getInterval().getStart();
     }
 
     public boolean hasTransitions() {
-        return mergedForecastList.size() > 1; // Because the first one is the current weather
+        return forecastList.size() > 1; // Because the first one is the current weather
     }
 
     /**
      * Returns the processed lists of forecasts. It is guaranteed to be non-empty.
      */
     public List<Forecast> getForecastList() {
-        return mergedForecastList;
+        return forecastList;
     }
 
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
-        for (Forecast forecast : mergedForecastList) {
+        for (Forecast forecast : forecastList) {
             builder.append("\n" + forecast);
         }
         return builder.toString();
