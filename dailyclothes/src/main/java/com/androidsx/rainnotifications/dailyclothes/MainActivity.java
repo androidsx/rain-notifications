ab0360cd9c66acde81fe6859f131f8fc976e9ca9
Josep Sansalvador Balaguer <josep@androidsx.com>
2014-10-01 19:31:21 +0200

    Moved quickReturn code into new class

--- dailyclothes/src/main/java/com/androidsx/rainnotifications/dailyclothes/MainActivity.java
+++ dailyclothes/src/main/java/com/androidsx/rainnotifications/dailyclothes/MainActivity.java
@@ -5,21 +5,17 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
-import android.view.animation.Animation;
-import android.view.animation.TranslateAnimation;
-import android.widget.AbsListView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.androidsx.rainnotifications.dailyclothes.model.Clothes;
+import com.androidsx.rainnotifications.dailyclothes.quickreturn.QuickReturn;
 import com.androidsx.rainnotifications.dailyclothes.quickreturn.QuickReturnListView;
 import com.androidsx.rainnotifications.dailyclothes.util.NotificationHelper;
 import com.androidsx.rainnotifications.dailyclothes.widget.CustomFontTextView;
@@ -36,22 +32,6 @@ public class MainActivity extends Activity {
     private QuickReturnListView mListView;
     private CustomFontTextView mQuickReturnView;
     private View mPlaceHolder;
-    private View listViewHeader;
-
-    private int mCachedVerticalScrollRange;
-    private int mQuickReturnHeight;
-
-    private static final int STATE_ONSCREEN = 0;
-    private static final int STATE_OFFSCREEN = 1;
-    private static final int STATE_RETURNING = 2;
-    private static final int STATE_EXPANDED = 3;
-    private int mState = STATE_ONSCREEN;
-    private int mScrollY;
-    private int mMinRawY = 0;
-    private int rawY;
-    private boolean noAnimation = false;
-
-    private TranslateAnimation anim;
 
     private int maxTemp = 0;
     private int todayNumClicks = 0;
@@ -70,10 +50,10 @@ public class MainActivity extends Activity {
         mQuickReturnView = (CustomFontTextView)findViewById(R.id.forecast_message);
         mPlaceHolder = findViewById(R.id.layout_weather);
         mListView = (QuickReturnListView)findViewById(R.id.clothes_list_view);
-        listViewHeader = LayoutInflater.from(this).inflate(R.layout.header, null);
-
+        
         fillForecastView(8, 24);
         fillClothesListView();
+        configureQuickReturn();
     }
 
     public void showNotification(View v) {
@@ -133,167 +113,14 @@ public class MainActivity extends Activity {
                 maxTemp = auxTemp;
             }
         }
-        mQuickReturnView.setText(Html.fromHtml(String.format(getString(R.string.forecast_first_message), maxTemp)));
+        ((TextView)findViewById(R.id.forecast_message)).setText(
+                Html.fromHtml(String.format(getString(R.string.forecast_first_message), maxTemp)));
     }
 
     private void fillClothesListView() {
         adapter = new CustomListAdapter(this, clothesList);
         mListView.setAdapter(adapter);
-        mListView.addHeaderView(listViewHeader);
-
-        mListView.getViewTreeObserver().addOnGlobalLayoutListener(
-                new ViewTreeObserver.OnGlobalLayoutListener() {
-                    @Override
-                    public void onGlobalLayout() {
-                        mQuickReturnHeight = mQuickReturnView.getHeight();
-                        mListView.computeScrollY();
-                        mCachedVerticalScrollRange = mListView.getListHeight();
-                    }
-                });
-
-        mListView.setOnScrollListener(new AbsListView.OnScrollListener() {
-            @Override
-            public void onScroll(AbsListView view, int firstVisibleItem,
-                                 int visibleItemCount, int totalItemCount) {
-
-                mScrollY = 0;
-                int translationY = 0;
-
-                if (mListView.scrollYIsComputed()) {
-                    mScrollY = mListView.getComputedScrollY();
-                }
-
-                rawY = mPlaceHolder.getTop()
-                        - Math.min(
-                        mCachedVerticalScrollRange
-                                - mListView.getHeight(), mScrollY);
-
-                switch (mState) {
-                    case STATE_OFFSCREEN:
-                        if (rawY <= mMinRawY) {
-                            mMinRawY = rawY;
-                        } else {
-                            mState = STATE_RETURNING;
-                        }
-                        translationY = rawY;
-                        break;
-
-                    case STATE_ONSCREEN:
-                        if (rawY < -mQuickReturnHeight) {
-                            System.out.println("test3");
-                            mState = STATE_OFFSCREEN;
-                            mMinRawY = rawY;
-                        }
-                        translationY = rawY;
-                        break;
-
-                    case STATE_RETURNING:
-
-                        if (translationY > 0) {
-                            translationY = 0;
-                            mMinRawY = rawY - mQuickReturnHeight;
-                        }
-
-                        else if (rawY > 0) {
-                            mState = STATE_ONSCREEN;
-                            translationY = rawY;
-                        }
-
-                        else if (translationY < -mQuickReturnHeight) {
-                            mState = STATE_OFFSCREEN;
-                            mMinRawY = rawY;
-
-                        } else if (mQuickReturnView.getTranslationY() != 0
-                                && !noAnimation) {
-                            noAnimation = true;
-                            anim = new TranslateAnimation(0, 0,
-                                    -mQuickReturnHeight, 0);
-                            anim.setFillAfter(true);
-                            anim.setDuration(250);
-                            mQuickReturnView.startAnimation(anim);
-                            anim.setAnimationListener(new Animation.AnimationListener() {
-
-                                @Override
-                                public void onAnimationStart(Animation animation) {
-                                    // TODO Auto-generated method stub
-
-                                }
-
-                                @Override
-                                public void onAnimationRepeat(Animation animation) {
-                                    // TODO Auto-generated method stub
-
-                                }
-
-                                @Override
-                                public void onAnimationEnd(Animation animation) {
-                                    noAnimation = false;
-                                    mMinRawY = rawY;
-                                    mState = STATE_EXPANDED;
-                                }
-                            });
-                        }
-                        break;
-
-                    case STATE_EXPANDED:
-                        if (rawY < mMinRawY - 2 && !noAnimation) {
-                            noAnimation = true;
-                            anim = new TranslateAnimation(0, 0, 0,
-                                    -mQuickReturnHeight);
-                            anim.setFillAfter(true);
-                            anim.setDuration(250);
-                            anim.setAnimationListener(new Animation.AnimationListener() {
-
-                                @Override
-                                public void onAnimationStart(Animation animation) {
-                                }
-
-                                @Override
-                                public void onAnimationRepeat(Animation animation) {
-
-                                }
-
-                                @Override
-                                public void onAnimationEnd(Animation animation) {
-                                    noAnimation = false;
-                                    mState = STATE_OFFSCREEN;
-                                }
-                            });
-                            mQuickReturnView.startAnimation(anim);
-                        } else if (translationY > 0) {
-                            translationY = 0;
-                            mMinRawY = rawY - mQuickReturnHeight;
-                        }
-
-                        else if (rawY > 0) {
-                            mState = STATE_ONSCREEN;
-                            translationY = rawY;
-                        }
-
-                        else if (translationY < -mQuickReturnHeight) {
-                            mState = STATE_OFFSCREEN;
-                            mMinRawY = rawY;
-                        } else {
-                            mMinRawY = rawY;
-                        }
-                }
-                /** this can be used if the build is below honeycomb **/
-                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.HONEYCOMB) {
-                    anim = new TranslateAnimation(0, 0, translationY,
-                            translationY);
-                    anim.setFillAfter(true);
-                    anim.setDuration(0);
-                    mQuickReturnView.startAnimation(anim);
-                } else {
-                    mQuickReturnView.setTranslationY(translationY);
-                }
-
-            }
-
-            @Override
-            public void onScrollStateChanged(AbsListView view, int scrollState) {
-            }
-        });
+        mListView.addHeaderView(LayoutInflater.from(this).inflate(R.layout.header, null));
 
         clothesList.add(new Clothes(R.drawable.lucky_1));
         clothesList.add(new Clothes(R.drawable.lucky_2));
@@ -317,6 +144,10 @@ public class MainActivity extends Activity {
         clothesList.add(new Clothes(R.drawable.blogger_10));
 
         adapter.notifyDataSetChanged();
+    }
+
+    private void configureQuickReturn() {
+        QuickReturn.configureQuickReturn(mQuickReturnView, mListView, mPlaceHolder);
     }
 
     private int getRandomBetweenNumbers(int minValue, int maxValue) {
