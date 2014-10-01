37e616db2d36655ec91b68606b1742ee4885ecab
Pablo Pera <pablo.sx@gmail.com>
2014-10-01 12:39:29 -0400

    Picasso for image loading

--- dailyclothes/src/main/java/com/androidsx/rainnotifications/dailyclothes/MainActivity.java
+++ dailyclothes/src/main/java/com/androidsx/rainnotifications/dailyclothes/MainActivity.java
@@ -31,6 +31,7 @@ import com.androidsx.rainnotifications.dailyclothes.model.Clothes;
 import com.androidsx.rainnotifications.dailyclothes.quickreturn.QuickReturnListView;
 import com.androidsx.rainnotifications.dailyclothes.util.NotificationHelper;
 import com.androidsx.rainnotifications.dailyclothes.widget.CustomFontTextView;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -134,7 +135,7 @@ public class MainActivity extends Activity {
             ImageView icon = (ImageView) view.findViewById(R.id.forecast_icon);
             TextView temp = (TextView) view.findViewById(R.id.forecast_temp);
             TextView hour = (TextView) view.findViewById(R.id.forecast_hour);
-            icon.setImageDrawable(getResources().getDrawable(getRandomWeatherIcon()));
+            Picasso.with(this).load(getRandomWeatherIcon()).into(icon);
             int auxTemp = getRandomBetweenNumbers(60, 67);
             temp.setText(auxTemp + "º");
             hour.setText(i+"am");
