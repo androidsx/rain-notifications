8ce0bbff6ee79c8e8e5a97ec00506e22c662533e
Josep Sansalvador Balaguer <josep@androidsx.com>
2014-10-01 14:29:07 +0200

    Removed rounded bitmap corners method

--- dailyclothes/src/main/java/com/androidsx/rainnotifications/dailyclothes/MainActivity.java
+++ dailyclothes/src/main/java/com/androidsx/rainnotifications/dailyclothes/MainActivity.java
@@ -342,28 +342,6 @@ public class MainActivity extends Activity {
         return mascotTypedArray.getResourceId(mascotIndex, -1);
     }
 
-    public static Bitmap getRoundedCornerBitmap(Bitmap bitmap) {
-        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
-                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(output);
-
-        final int color = 0xff424242;
-        final Paint paint = new Paint();
-        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
-        final RectF rectF = new RectF(rect);
-        final float roundPx = 15;
-
-        paint.setAntiAlias(true);
-        canvas.drawARGB(0, 0, 0, 0);
-        paint.setColor(color);
-        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
-
-        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-        canvas.drawBitmap(bitmap, rect, rect, paint);
-
-        return output;
-    }
-
     public static class CustomListAdapter extends BaseAdapter {
         private Context context;
         private LayoutInflater inflater;
