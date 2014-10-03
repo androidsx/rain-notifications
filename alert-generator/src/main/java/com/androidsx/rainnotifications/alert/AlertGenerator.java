a8d006c1329cbfe047638067d7fce092839a3f2d
Omar Pera <campbell.sx@gmail.com>
2014-10-03 14:36:46 -0400

    Remove ugly logs

--- alert-generator/src/main/java/com/androidsx/rainnotifications/alert/AlertGenerator.java
+++ alert-generator/src/main/java/com/androidsx/rainnotifications/alert/AlertGenerator.java
@@ -21,8 +21,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Random;
 
-import timber.log.Timber;
-
 /**
  * Generates weather alerts that are relevant to the user.
  * <p/>
@@ -114,8 +112,6 @@ public class AlertGenerator {
     private int generateMascot(WeatherType weather) {
         final List<Integer> mascotsForThisWeather = new ArrayList<Integer>();
         for (WeatherTypeMascots wtm : mascots) {
-            Timber.d(""+ weather);
-            Timber.v("" + wtm);
             if (wtm.getType().equals(weather)) {
                 for (String s : wtm.getDressedMascots()) {
                     mascotsForThisWeather.add(context.getResources().getIdentifier(s, "drawable", context.getPackageName()));
