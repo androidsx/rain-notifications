4f204c2779ab370ba5dc3dd3432ad059296dacff
parodco <parodco@gmail.com>
2014-10-07 19:58:11 +0200

    Before renace classes and refactor

---
+++ model/src/main/java/com/androidsx/rainnotifications/model/DayTemplate.java
@@ -0,0 +1,14 @@
+package com.androidsx.rainnotifications.model;
+
+public class DayTemplate {
+
+    public boolean match(DaySummary day) {
+        return false;
+    }
+
+    public String resolveMessage(DaySummary day) {
+        return "";
+    }
+
+
+}
