f0bf723bb49738e895dbd15d9b84bb6972f944e1
parodco <parodco@gmail.com>
2014-11-19 19:49:58 +0100

    Implementing Prototype

--- dailyclothes/src/main/java/com/androidsx/rainnotifications/dailyclothes/MainActivityTest.java
+++ dailyclothes/src/main/java/com/androidsx/rainnotifications/dailyclothes/MainActivityTest.java
@@ -1,5 +1,7 @@
 package com.androidsx.rainnotifications.dailyclothes;
 
+import android.content.res.TypedArray;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -11,28 +13,163 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.androidsx.rainnotifications.alert.DayTemplateGenerator;
+import com.androidsx.rainnotifications.backgroundservice.util.UserLocationFetcher;
 import com.androidsx.rainnotifications.dailyclothes.model.Clothes;
+import com.androidsx.rainnotifications.dailyclothes.widget.CustomFontTextView;
+import com.androidsx.rainnotifications.forecastapislibrary.WeatherClientException;
+import com.androidsx.rainnotifications.forecastapislibrary.WeatherClientResponseListener;
+import com.androidsx.rainnotifications.model.Day;
+import com.androidsx.rainnotifications.model.DayTemplate;
+import com.androidsx.rainnotifications.model.DayTemplateLoaderFactory;
+import com.androidsx.rainnotifications.model.Forecast;
+import com.androidsx.rainnotifications.model.ForecastTable;
+import com.androidsx.rainnotifications.model.WeatherType;
+import com.androidsx.rainnotifications.model.WeatherWrapper;
+import com.androidsx.rainnotifications.model.util.UiUtil;
+import com.androidsx.rainnotifications.weatherclientfactory.WeatherClientFactory;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
+import com.squareup.picasso.Picasso;
 
+import org.joda.time.DateTime;
+import org.joda.time.Duration;
+
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+
+import timber.log.Timber;
 
 
 public class MainActivityTest extends FragmentActivity {
 
-    private SlidingUpPanelLayout bottomSheet;
-    private ViewPager imagesPager;
+    private static final Duration EXPIRATION_DURATION = Duration.standardSeconds(5); // TODO: Use this Duration.standardHours(1)
+    private static final int MAX_FORECAST_ITEMS = 24;
+    private static final String TEMPERATURE_SYMBOL = "°";
+
+    private enum ForecastDataState {LOADING, ERROR, DONE};
+
+    private WeatherWrapper.TemperatureScale localeScale;
+    private DecimalFormat temperatureFormat = new DecimalFormat("#");
+    private ForecastDataState dataState;
+    private ForecastTable forecastTable;
+    private DateTime forecastTableTime;
+    private Day day;
+    private String forecastMessage;
     private MainImagePagerAdapter adapter;
     private List<Clothes> clothesList = new ArrayList<Clothes>();
+
+    private boolean destroyed = false;
+    private View frameMain;
+    private View frameLoading;
+    private View frameError;
+    private CustomFontTextView nowTemperature;
+    private CustomFontTextView minTemperature;
+    private CustomFontTextView maxTemperature;
+    private SlidingUpPanelLayout bottomSheet;
+    private ViewPager imagesPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        localeScale  = WeatherWrapper.TemperatureScale.getLocaleScale(this);
+        setupUI();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        checkDataState();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        destroyed = true;
+    }
+
+    private void checkDataState() {
+        if(dataState == null || dataState.equals(ForecastDataState.ERROR)) {
+            setForecastDataState(ForecastDataState.LOADING);
+        }
+        else if(dataState.equals(ForecastDataState.DONE) && new Duration(forecastTableTime, new DateTime()).isLongerThan(EXPIRATION_DURATION)) {
+            setForecastDataState(ForecastDataState.LOADING);
+        }
+    }
+
+    private void setForecastDataState(ForecastDataState newState) {
+        dataState = newState;
+        switch (newState) {
+            case LOADING:
+                forecastTable = null;
+                forecastTableTime = null;
+                day = null;
+                forecastMessage = null;
+                getForecastData();
+                break;
+            case ERROR:
+                break;
+            case DONE:
+                break;
+        }
+
+        updateUI();
+    }
+
+    private void getForecastData() {
+        // FIXME: we do exactly the same in the weather service. grr..
+        UserLocationFetcher.getUserLocation(this, new UserLocationFetcher.UserLocationResultListener() {
+            @Override
+            public void onLocationSuccess(final Location location) {
+                WeatherClientFactory.requestForecastForLocation(MainActivityTest.this, location.getLatitude(), location.getLongitude(), new WeatherClientResponseListener() {
+                    @Override
+                    public void onForecastSuccess(ForecastTable forecastTable) {
+                        MainActivityTest.this.forecastTable = forecastTable;
+                        MainActivityTest.this.forecastTableTime = new DateTime();
+                        MainActivityTest.this.day = new Day(forecastTable);
+
+                        DayTemplate template = new DayTemplateGenerator(DayTemplateLoaderFactory.getDayTemplateLoader(MainActivityTest.this)).getDayTemplate(day);
+                        if (template == null) {
+                            MainActivityTest.this.forecastMessage = getString(R.string.default_day_message);
+                        } else {
+                            MainActivityTest.this.forecastMessage = template.resolveMessage(MainActivityTest.this, MainActivityTest.this.day);
+                        }
+
+                        setForecastDataState(ForecastDataState.DONE);
+                    }
+
+                    @Override
+                    public void onForecastFailure(WeatherClientException exception) {
+                        Timber.e(exception, "Failed to get the forecast");
+                        setForecastDataState(ForecastDataState.ERROR);
+                    }
+                });
+            }
+
+            @Override
+            public void onLocationFailure(UserLocationFetcher.UserLocationException exception) {
+                Timber.e(exception, "Failed to get the location");
+                setForecastDataState(ForecastDataState.ERROR);
+            }
+        });
+    }
+
+    private void setupUI() {
+        // TODO: Enable support email link.
         setContentView(R.layout.activity_main_activity_test);
 
-        imagesPager = (ViewPager) findViewById(R.id.view_pager);
+        frameMain = findViewById(R.id.frame_main);
+        frameLoading = findViewById(R.id.frame_loading);
+        frameError = findViewById(R.id.frame_error);
         bottomSheet = (SlidingUpPanelLayout) findViewById(R.id.sliding_layout);
+
+        imagesPager = (ViewPager) frameMain.findViewById(R.id.view_pager);
+        nowTemperature = (CustomFontTextView) findViewById(R.id.now_temp);
+        minTemperature = (CustomFontTextView) findViewById(R.id.today_min_temp);
+        maxTemperature = (CustomFontTextView) findViewById(R.id.today_max_temp);
+
 
         bottomSheet.setPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener() {
             @Override
@@ -61,7 +198,87 @@ public class MainActivityTest extends FragmentActivity {
             }
         });
 
+        frameError.findViewById(R.id.button_error_retry).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setForecastDataState(ForecastDataState.LOADING);
+            }
+        });
+
         fillClothesViewPager();
+    }
+
+    private void updateUI() {
+        // FIXME: La fuente utilizada muestra los símbolos de temperatura en negrita, Aghhh!
+        if(!destroyed) {
+            switch (dataState) {
+                case LOADING:
+                    frameLoading.setVisibility(View.VISIBLE);
+
+                    frameError.setVisibility(View.INVISIBLE);
+                    frameMain.setVisibility(View.INVISIBLE);
+                    break;
+                case ERROR:
+                    frameError.setVisibility(View.VISIBLE);
+
+                    frameLoading.setVisibility(View.INVISIBLE);
+                    frameMain.setVisibility(View.INVISIBLE);
+                    break;
+                case DONE:
+                    frameMain.setVisibility(View.VISIBLE);
+
+                    frameLoading.setVisibility(View.INVISIBLE);
+                    frameError.setVisibility(View.INVISIBLE);
+
+                    nowTemperature.setText(temperatureFormat.format(forecastTable.getBaselineForecast().getWeatherWrapper().getTemperature(localeScale)) + TEMPERATURE_SYMBOL);
+                    minTemperature.setText(temperatureFormat.format(day.getMinTemperature().getWeatherWrapper().getTemperature(localeScale)));
+                    maxTemperature.setText(temperatureFormat.format(day.getMaxTemperature().getWeatherWrapper().getTemperature(localeScale)));
+                    ((TextView)findViewById(R.id.forecast_message)).setText(forecastMessage); //TODO: Utilizar variable para esto
+                    fillForecastView();
+
+
+                    break;
+            }
+        }
+    }
+
+    private void fillForecastView() {
+        ViewGroup forecastView = (ViewGroup)findViewById(R.id.hourly_forecast);
+        forecastView.removeAllViews();
+
+        for(int i=0; i < Math.min(MAX_FORECAST_ITEMS, forecastTable.getHourlyForecastList().size()); i++) {
+            Forecast current = forecastTable.getHourlyForecastList().get(i);
+
+            View view = LayoutInflater.from(this).inflate(R.layout.hourly_forecast_item, null);
+            ImageView icon = (ImageView) view.findViewById(R.id.forecast_icon);
+            TextView temp = (TextView) view.findViewById(R.id.forecast_temp);
+            TextView hour = (TextView) view.findViewById(R.id.forecast_hour);
+
+            Picasso.with(this).load(getWeatherIcon(current.getWeatherWrapper().getWeatherType())).into(icon);
+            temp.setText(temperatureFormat.format(current.getWeatherWrapper().getTemperature(localeScale)) + TEMPERATURE_SYMBOL);
+            hour.setText(UiUtil.getReadableHour(current.getInterval().getStart()));
+
+            forecastView.addView(view);
+        }
+    }
+
+    private int getWeatherIcon(WeatherType type) {
+        TypedArray iconTypedArray = getResources().obtainTypedArray(R.array.weatherIcons);
+
+        switch (type) {
+            case CLEAR:
+                return iconTypedArray.getResourceId(0,0); //TODO: Que usamos como default?
+            case CLOUDY:
+                return iconTypedArray.getResourceId(1,0);
+            case RAIN:
+                return iconTypedArray.getResourceId(2,0);
+            case SNOW:
+                // FIXME: Falta el icono
+                return iconTypedArray.getResourceId(3,0);
+            default:
+                // TODO: Consultar con Pablo y/o Omar que hacer en este caso.
+                return 0;
+        }
     }
 
     private void fillClothesViewPager() {
